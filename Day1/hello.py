98b925b00156d08f323629fbe296e102ceb34458
MH <779605603@qq.com>
2020-05-22 23:05:47 +0800

    运行第一个pytho程序&输入与输出

---
+++ Day1/hello.py
@@ -0,0 +1,15 @@
+#python中的输出
+#可以直接打印字符串
+print('Hello Python')
+#自动拼接字符串
+print('ZQ','LOVE','MH')
+#打印计算值
+print("100+200=",100+200)
+
+#python中的输入,加逗号
+name = input('请输入你的名字：')
+print("Hello",name)
+
+#python中的输入，加加号
+name = input('请输入你的名字：')
+print("Hello"+name)
